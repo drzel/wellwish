c10ec6e8749e3bbe94ad41575abc4f3cca3702c8
drzel <shayolden@hotmail.com>
2016-08-12 13:41:54 +0800

    Add wishes to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  Wish.create(
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+  )
+end
